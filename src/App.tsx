50759a4e230b76b9561138a5a9ef0188c76bfa89
Andre Cristiano <andre.cristiano1975@gmail.com>
2024-05-31 17:09:57 -0300

    conf: install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import '@/global.css'
+import { Button } from './components/ui/button'
+
 export function App() {
 
   return (
-    <>
+    <div className='flex justify-center items-center h-screen'>
       <h1>Pizza Shop</h1>
-    </>
+      <Button>Enviar</Button>
+    </div>
   )
 }
