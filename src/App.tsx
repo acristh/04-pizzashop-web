586ec6e489b60d0151563f9f15d93f2856d5fd48
Andre Cristiano <andre.cristiano1975@gmail.com>
2024-06-02 07:24:25 -0300

    conf: install React Router Dom and configurate the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import '@/global.css'
-import { Button } from './components/ui/button'
+
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-
-  return (
-    <div className='flex justify-center items-center h-screen'>
-      <h1>Pizza Shop</h1>
-      <Button>Enviar</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
